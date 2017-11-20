ea7ba90dae1b43783e2dc148edf5a8705c52d7ef
betsy18 <betsy_dessire@hotmail.com>
2017-11-20 01:03:17 -0500

    Agregando texto a un parrafo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-window.load() {
+window.addEventListener('load', function(event) {
+  var textArea = document.getElementById('textArea');
+  var save = document.getElementById('save');
+  var character = document.getElementById('character');
+  var container = document.getElementById('container');
 
-}
+  save.addEventListener('click', function(event) {
+    // para verificar que tenga contenido 
+    if (textArea.value) {
+      var text = document.createElement('p');
+      text.textContent = textArea.value;
+      container.appendChild(text);
+      console.log(text);
+      text.value = '';
+      container.classList.add('form');
+    }
+  });
+});
